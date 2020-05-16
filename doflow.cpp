a2cdeb30f6b791e388919a678e547e3ea057950a
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-05-16 07:11:04 +0200

    Changes 2020/05/16-07:11:04
    Change-Id: Ie62b9fe95673325fffd78f9bb57436672e45542e

--- doflow.cpp
+++ doflow.cpp
@@ -4,60 +4,73 @@
 #define GRID_SIZE 20
 #define AMPLIFICATION 1
 
-class ChangeDetector{
+class DenseFlow {
 private:
 	cv::Mat flow, prev, next, colored;
 	cv::VideoCapture cap;
 	int channel=-1;
 	std::string vidflow;
-	bool active=true;
+	bool active=true, draw=false;
 	float fps;
 	cv::Size size;
-	void initialize(){
-		if(channel>=0) cap.open(channel); else cap.open(vidflow);
+	void initialize() {
+		if(channel>=0) cap.open(channel);
+		else cap.open(vidflow);
 		cap >> prev;
 		cvtColor(prev, prev, cv::COLOR_BGR2GRAY);
 		fps=cap.get(cv::CAP_PROP_FPS);
 		size=cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
 	}
 public:
-	ChangeDetector(std::string f): vidflow(f) { initialize(); }
-	ChangeDetector(int c): channel(c)         { initialize(); }
-	void processnext(){
+	DenseFlow(std::string f): vidflow(f) { initialize(); }
+	DenseFlow(int c): channel(c)         { initialize(); }
+	void drawVectors() {
+		for(int y=0; y<next.rows; y+=GRID_SIZE) {
+			for(int x=0; x<next.cols; x+=GRID_SIZE) {
+				const cv::Point2f flowatxy = flow.at<cv::Point2f>(y, x) * 1;
+				line(colored, cv::Point(x, y), cv::Point(cvRound(x + flowatxy.x*AMPLIFICATION), cvRound(y + flowatxy.y*AMPLIFICATION)), cv::Scalar(0, 255, 0));
+				circle(colored, cv::Point(x, y), 1, cv::Scalar(0, 0, 0), -1);
+			}
+		}
+	}
+	void calculateFlow() {
 		cap >> next;
 		if(next.empty()) { cap.release(); active=false; } else {
 			next.copyTo(colored);
 			cvtColor(next, next, cv::COLOR_BGR2GRAY);
 			cv::calcOpticalFlowFarneback(prev, next, flow, 0.4, 1, 12, 2, 8, 1.2, 0);
-			for(int y=0; y<next.rows; y+=GRID_SIZE) {
-				for(int x=0; x<next.cols; x+=GRID_SIZE) {
-					const cv::Point2f flowatxy = flow.at<cv::Point2f>(y, x) * 1;
-					line(colored, cv::Point(x, y), cv::Point(cvRound(x + flowatxy.x*AMPLIFICATION), cvRound(y + flowatxy.y*AMPLIFICATION)), cv::Scalar(0, 255, 0));
-					circle(colored, cv::Point(x, y), 1, cv::Scalar(0, 0, 0), -1);
-				}
-			}
+			if(draw) drawVectors();
 			next.copyTo(prev);
-			imshow("OpticalFlow", colored);
 		}
 	}
 	float getFPS()      { return fps; }
 	bool isActive()     { return active; }
 	cv::Size getSize()  { return size; }
-	cv::Mat  getImage() { return colored; }
+	cv::Mat& getImage() { return colored; }
+	cv::Mat& getFlow()  { return flow; }
 	cv::VideoCapture& getVideoCapture() { return cap; }
+	void setDraw(bool state) { draw=state; }
 };
 
 int main(int argc, char* argv[]) {
+	DenseFlow dflow(0); // dflow(std::string(argv[1]));
+
+	// Vectors drawing
 	cv::namedWindow("OpticalFlow", cv::WINDOW_AUTOSIZE);
-	ChangeDetector cd(0); // cd(std::string(argv[1]));
+	dflow.setDraw(true);
 
-	// Video generation: uncomment writers
+	// Video generation
 	// cv::VideoWriter writer;
-	// writer.open("output.avi", CV_FOURCC('M', 'J', 'P', 'G'), cd.getFPS(), cd.getSize());
+	// writer.open("output.avi", CV_FOURCC('M', 'J', 'P', 'G'), dflow.getFPS(), dflow.getSize());
 
-	while(cv::waitKey(1)!=113 && cd.isActive()) {
-		cd.processnext();
-		// writer<<cd.getImage();
+	while(cv::waitKey(1)!=113 && dflow.isActive()) {
+		dflow.calculateFlow();
+
+		// Vectors drawing
+		imshow("OpticalFlow", dflow.getImage());
+
+		// Video generation
+		// writer<<dflow.getImage();
 	}
 	return 0;
 }
